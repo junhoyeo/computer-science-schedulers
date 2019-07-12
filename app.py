6cd9a7e62fef5162f42fab7bccef887c572da3e6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2019-07-12 11:25:39 +0900

    Calculate AWT, ATT

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ with open('./process.json') as proc_file:
   procs = json.load(proc_file)
 
 for proc in procs:
-  proc['start'] = 0
-  proc['delay'] = 0
+  proc['started'] = 0
+  proc['terminated'] = 0
 
 print(procs)
 
@@ -21,8 +21,9 @@ while True:
 
   # 종료 프로세스 확인
   if current: 
-    if current['execution'] + current['start'] == time:
+    if current['execution'] + current['started'] == time:
       print('p{} terminated'.format(current['id']))
+      current['terminated'] = time
       current = None
 
   # 현재 프로세스 확인
@@ -32,6 +33,21 @@ while True:
     except:
       break
     print('p{} started'.format(current['id']))
-    current['start'] = time
+    current['started'] = time
 
   time += 1
+
+print('\n=== results ===')
+awt = 0
+att = 0
+for proc in procs:
+  print('#p{}'.format(proc['id']), end='')
+  wt = proc['started'] - proc['arrival']
+  print(', wait time: {}'.format(wt), end='') # 대기 시간
+  tt = proc['terminated'] - proc['arrival']
+  print(', termination time: {}'.format(tt)) # 반환 시간
+  awt += wt
+  att += tt
+awt /= len(procs)
+att /= len(procs)
+print('\nAWT: {}\nATT: {}'.format(awt, att))
